fe83484edc89ae67d7b85f15cad71ed254263db8
kareem badawy <kareembadawy94@gmail.com>
2020-01-31 20:47:44 +0200

    DP-7 Structural Decorator Pattern

--- src/Structural/index.php
+++ src/Structural/index.php
@@ -1,3 +1,16 @@
 <?php
+
+use DesignPatternsInPHP\Structural\Decorator\Parcel;
+use DesignPatternsInPHP\Structural\Decorator\InternationalShipping;
  
 require __DIR__ . './../../vendor/autoload.php';
+
+function decorator()
+{
+    $parcel = new Parcel(10, 'A Supreme t-shirt');
+    $parcel = new InternationalShipping($parcel);
+
+    echo "{$parcel->getDescription()} for \${$parcel->calculatePrice()}.";
+}
+
+decorator();
