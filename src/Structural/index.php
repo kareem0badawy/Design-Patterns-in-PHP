d421cb9b7a209acf07f7007c90761c2729503472
kareem badawy <kareembadawy94@gmail.com>
2020-02-01 12:24:51 +0200

    DP- 11 Facade Pattern - another example turn on and of computer

--- src/Structural/index.php
+++ src/Structural/index.php
@@ -9,7 +9,9 @@ use DesignPatternsInPHP\Structural\Facade\Facebook;
 use DesignPatternsInPHP\Structural\Decorator\Parcel;
 use DesignPatternsInPHP\Structural\Adapter\PayPalAdapter;
 use DesignPatternsInPHP\Structural\Adapter\StripeAdapter;
+use DesignPatternsInPHP\Structural\Facade\anotherOne\Computer;
 use DesignPatternsInPHP\Structural\Decorator\InternationalShipping;
+use DesignPatternsInPHP\Structural\Facade\anotherOne\ComputerFacade;
  
 require __DIR__ . './../../vendor/autoload.php';
 
@@ -34,6 +36,8 @@ function adapter()
 
 // adapter();
 
+
+/*********************************Begin Facade************************************ */
 function facade()
 {
     $message = 'New video is here!';
@@ -44,4 +48,17 @@ function facade()
     Share::to($facebook, $twitter, null);
 }
 
-facade();
+// facade();
+
+//  another one example
+
+function turnOnAndOfComputer()
+{
+    $computer = new ComputerFacade(new Computer());
+    $computer->turnOn(); echo '<br>'; // Ouch! Beep beep! Loading.. Ready to be used!
+    $computer->turnOff(); // Bup bup buzzz! Haah! Zzzzz
+}
+
+turnOnAndOfComputer();
+
+/*********************************End Facade************************************ */
