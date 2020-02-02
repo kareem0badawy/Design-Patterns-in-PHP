df6e6929f10e8dd8ed02e90f967956f15c19900e
kareem badawy <kareembadawy94@gmail.com>
2020-02-02 20:55:54 +0200

    DP-13 Observer Pattern - Notifications, similar to event-listeners

--- src/Behavioral/index.php
+++ src/Behavioral/index.php
@@ -1,3 +1,19 @@
 <?php
- 
+
+use DesignPatternsInPHP\Behavioral\Observer\Login;
+use DesignPatternsInPHP\Behavioral\Observer\NotifierObserver;
+
 require __DIR__ . './../../vendor/autoload.php';
+
+/********************************* Begin Observer ************************************ */
+function observer()
+{
+    $login = new Login();
+    $login->attach(new NotifierObserver());
+    $login->login([
+        'email' => 'john@doe.com',
+        'password' => 'secret'
+    ]);
+}
+observer();
+/********************************* End Observer ************************************ */
